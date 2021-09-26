432880748f7fbe3577388acb058b2a170066e4db
Alexey Popov <alexnn0987@mail.ru>
2021-09-26 23:53:12 +0300

    Мой учебный коммит

---
+++ Lesson_1/Class_Work/index.js
@@ -0,0 +1,22 @@
+let namePlanet = 'Земля';
+console.log(namePlanet);
+
+let currentTime = 2021;
+console.log(currentTime);
+
+let quantityArticle = 5;
+console.log(quantityArticle);
+
+let isPaymentCash = 'да';
+console.log(isPaymentCash);
+
+let userSurname = 'Popov';
+console.log(userSurname);
+
+let userName = 'Alexey';
+console.log(userName);
+
+let userPatronymic = 'Alexandrovich';
+console.log(userPatronymic);
+
+console.log('hello world');
---
+++ Lesson_1/Home_work/index.js
@@ -0,0 +1,75 @@
+// 1) Вывести в терминал строку “Hello World!”
+// let hello = 'Hello World!';
+// console.log(hello);
+
+// 2) Создать переменную для каждого типа данных
+// 3) Вывести в консоль редактора кода созданные переменные
+// const num = 7;
+// let numBig = 13n;
+// let word = 'Строка';
+// let truth = 7 < 13;
+// let numPhone = null;
+// let user;
+// let guest = new Object();
+// let id = Symbol('id');
+
+// console.log(num);
+// console.log(numBig);
+// console.log(word);
+// console.log(truth);
+// console.log(numPhone);
+// console.log(user);
+// console.log(guest);
+// console.log(id);
+
+// 4) Вывести ее тип через оператор typeof и функцию typeof
+// const num = 7;
+// let numBig = 13n;
+// let word = 'Строка';
+// let truth = 7 < 13;
+// let numPhone = null;
+// let user;
+// let guest = new Object();
+// let id = Symbol('id');
+
+// console.log(typeof num);
+// console.log(typeof numBig);
+// console.log(typeof word);
+// console.log(typeof truth);
+// console.log(typeof numPhone);
+// console.log(typeof user);
+// console.log(typeof guest);
+// console.log(typeof id);
+
+// 5) Попробовать изменить переменную объявленную через const
+// let userName = 'Tom';
+// userName = 'Jek';
+// console.log(userName);
+// const numLittle = 1;
+// numLittle = 2;
+// console.log(numLittle);
+
+// 6) Попробовать изменить переменную с типом object объявленную через const
+// const user = {
+//   nameUser: 'Tom',
+// };
+// user.nameUser = 'Bob';
+// console.log(user.nameUser);
+
+// 7) Попробовать изменить переменную с типом object объявленную через let
+// let user = {
+//   nameUser: 'Jan',
+// };
+// user.nameUser = 'Robi';
+// console.log(user.nameUser);
+
+// 8) Попробовать изменить переменную с типом object объявленную через var
+// var user = {
+//   nameUser: 'Den',
+// };
+// user.nameUser = 'Ben';
+// console.log(user.nameUser);
+
+// 9) Установить редактор кода VS Code
+
+// 10) Ознакомиться с системой контроля версий GitHub (завести аккаунт)
