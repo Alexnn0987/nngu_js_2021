3d9b34d5d66c8848cb7a60138e206c27bf61a8f4
Alexey Popov <alexnn0987@mail.ru>
2021-10-03 15:23:09 +0300

    Мой учебный коммит

---
+++ lesson_3/Class_Work/index.js
@@ -0,0 +1,181 @@
+// STRING
+
+// const firstValue = String(5);
+
+// console.log(typeof firstValue);
+////////////////////////////////////////////////
+
+// const obj = {
+//   foo: 'bar',
+// };
+
+// console.log(typeof String(obj));
+//////////////////////////////////////////////////
+
+// const obj = {
+//   foo: 67,
+// };
+
+// console.log(typeof String(obj.foo));
+//////////////////////////////////////////////////////
+
+// NUMBER
+
+// let testValue = '12345';
+
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+
+// console.log(typeof testValue);
+///////////////////////////////////////
+
+// let testValue = '12345ZXC';
+
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+
+// console.log(typeof testValue);
+////////////////////////////////////////
+
+// let testValue;
+
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+
+// console.log(testValue);
+//////////////////////////////////////////////////
+
+// const isUser = true;
+// const isAdmin = false;
+
+// console.log(Number(isUser), Number(isAdmin));
+//////////////////////////////////////////////////
+
+// const voidString = '';
+
+// console.log(Number(voidString));
+//////////////////////////////////////////////////
+
+//BOOOLEAN
+
+// const value = '12';
+
+// console.log(typeof Boolean(value), Boolean(value));
+///////////////////////////////////////////////////////
+
+// const value = '';
+
+// console.log(typeof Boolean(value), Boolean(value));
+//////////////////////////////////////////////////////
+
+// const value = 0;
+
+// console.log(typeof Boolean(value), Boolean(value));
+/////////////////////////////////////////////////////////
+
+// const value = 30;
+
+// console.log(typeof Boolean(value), Boolean(value));
+//////////////////////////////////////////////////////////
+
+// const value = '';
+
+// console.log(typeof Boolean(value), Boolean(value));
+//////////////////////////////////////////////////////////
+
+// const obj = {};
+// console.log(Boolean(obj));
+///////////////////////////////////////////////
+
+// const obj = [];
+// console.log(Boolean(obj));
+////////////////////////////////////////////////
+
+// const obj = [].length;
+// console.log(Boolean(obj));
+////////////////////////////////////////////////
+
+// const obj = [1].length;
+// console.log(Boolean(obj));
+/////////////////////////////////////////////////
+
+// Математические операторы
+// console.log(16 % 5);
+
+// console.log(2 ** 2);
+
+// Ударный плюс
+// const value = 5;
+// console.log(typeof value, typeof +value);
+////////////////////////////////////////////////
+
+// const value = '5';
+// console.log(typeof value, typeof +value);
+//////////////////////////////////////////////////////////////////////
+
+// const value = '5';
+// console.log(typeof value, typeof +value, typeof Number(value));
+/////////////////////////////////////////////////////////////////////
+
+// Бинарный плюс
+
+// const sum = 5 + 6;
+// console.log(sum);
+///////////////////////////
+
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = 'Alex';
+// const numberValue = 34;
+
+// console.log(stringValue + numberValue);
+// console.log(numberValue + stringValue);
+/////////////////////////////////////////////////////////
+
+// const stringValue = 'Alex';
+// const numberValue = 34;
+
+// console.log(stringValue + numberValue);
+// console.log(typeof (numberValue + stringValue));
+//////////////////////////////////////////////////////////
+
+// const stringValue = '12';
+// const numberValue = 34;
+
+// console.log(stringValue + numberValue);
+// console.log(typeof (numberValue + stringValue));
+/////////////////////////////////////////////////////////
+
+// const stringValue = '12';
+// const numberValue = 34;
+
+// console.log(+stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+////////////////////////////////////////////////////////////
+
+// const stringValue = '12x';
+// const numberValue = 34;
+
+// console.log(+stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+///////////////////////////////////////////////////////
+
+// const firstName = 'Alex';
+// const lastName = 'Popov';
+
+// console.log(firstName + ' ' + lastName);
+
+// console.log(`${firstName} ${lastName}`);
+/////////////////////////////////////////////////////
+
+// Операторы сравнения
