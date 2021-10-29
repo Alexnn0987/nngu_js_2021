59bbcdb2b583ec570ef0a6ad3fe2cea25f24a8bc
Alexey Popov <alexnn0987@mail.ru>
2021-10-30 01:46:52 +0300

    Примитивы. Объекты.

--- lesson_6/Home_work/index.js
+++ lesson_6/Home_work/index.js
@@ -78,7 +78,7 @@
 //   });
 // };
 // console.log(change_register(str));
-// //  кАжДыЙ оХоТнИк ЖеЛаЕт ЗнАтЬ
+//  кАжДыЙ оХоТнИк ЖеЛаЕт ЗнАтЬ
 
 ////////////////////////////////////////////////////////////
 // 7. Удалить все не буквенно-цифровые символы
@@ -126,57 +126,77 @@
 // Напишите функцию insensitive_search(str1, str2), которая осуществляет поиск подстроки str2 в строкеstr1 без учёта регистра символов.
 
 // let insensitive_search = (str1, str2) => {
-//   return str1.includes(str2);
+//   return str1.toUpperCase().includes(str2.toUpperCase());
 // };
-// console.log(insensitive_search('Привет, Alex', 'Alex'));    //  true
-// console.log(insensitive_search('Привет, Alex', 'AlexEY'));  //  false
+// console.log(insensitive_search('Привет, Alex', 'Alex')); //  true
+// console.log(insensitive_search('Привет, Alex', 'AlexEY')); //  false
 
 ////////////////////////////////////////////////////////////
 // 11. ВерблюжийРегистр (CamelCase)
 // Напишите функцию initCap(str), которая преобразует стиль написания составных слов строки в CamelCase, при котором несколько слов пишутся слитно без пробелов, при этом каждое слово внутри строки пишется с заглавной буквы.
 
-// str = "hEllo woRld";
-// function initCap(str) {
-//     ...ваш код...
+// str = 'hEllo woRld';
+// let initCap = (str) => {
+//   return str.toLowerCase().replace(/(^|\s)\w/g, (str1) => {
+//     return str1.toUpperCase().replace(/\s+/g, '');
+//   });
 // };
+// console.log(initCap(str));   //  HelloWorld
 
 ////////////////////////////////////////////////////////////
 // 12. Змеиный_регистр (snake_case)
 // Напишите функцию initSnake(str), которая преобразует стиль написания составных слов строки из CamelCase в snake_case, при котором несколько слов разделяются символом подчеркивания (_), причём каждое слово пишется с маленькой буквы.
 
-// function initSnake(str) {
-//     ...ваш код...
+// let initSnake = (str) => {
+//   let lowerStr = str.replace(/[A-Z]/g, (str1) => {
+//     return '_' + str1.toLowerCase();
+//   });
+//   return lowerStr.replace(/^_/, '');
 // };
+// console.log(initSnake('HelloWorld')); //  hello_world
 
 ////////////////////////////////////////////////////////////
 // 13. Повторить строку n раз
 // Напишите функцию repeatStr(str, n), которая возвращает строку повторяемую определённое количество раз.
 
-// function repeatStr(str, n) {
-//     ...ваш код...
+// let repeatStr = (str, n) => {
+//   let newStr = '';
+//   for (let i = 0; i < n; i += 1) {
+//     newStr += str;
+//   }
+//   return newStr;
 // };
+
+// console.log(repeatStr('Hello*', 3));  //  Hello*Hello*Hello*
 
 ////////////////////////////////////////////////////////////
 // 14. Получить имя файла
 // Напишите функцию path(pathname), которая возвращает имя файла (подстрока после последнего символа "\" ) из полного пути к файлу.
 
-// var pathname = "/home/user/dir/file.txt";
-
-// function path(pathname) {
-//     ...ваш код...
+// let pathname = '/home/user/dir/file.txt';
+// let path = (pathname) => {
+//   return pathname.split('/').pop();
 // };
+// console.log(path(pathname)); //  file.txt
 
 ////////////////////////////////////////////////////////////
 // 15. Заканчивается ли строка символами другой строки
 // Создайте метод объекта String endsWith(), который сравнивает подстроку str1 с окончанием исходной строки str и определяет заканчивается ли строка символами подстроки.
 
-// var str = "Каждый охотник желает знать";
-// var str1 = "скрипт";
-// var str2 = "знать";
+// let str = 'Каждый охотник желает знать';
+// let str1 = 'скрипт';
+// let str2 = 'знать';
 
-// String.prototype.endsWith = function(substring) {
-//     ...ваш код...
+// String.prototype.endsWith = (substring) => {
+//   let elStr = String(str.match(/(\S+)$/g));
+//   if (elStr === substring) {
+//     return true;
+//   } else {
+//     return false;
+//   }
 // };
+// console.log(String.prototype.endsWith(str1));  // false
+// console.log(String.prototype.endsWith(str2));  // rtue
 
 ////////////////////////////////////////////////////////////
 // 16. Подстрока до/после указанного символа
@@ -217,28 +237,32 @@
 // 21. Удалить лишние пробелы из строки
 // Напишите функцию strip(str), которая удаляет все лишние пробелы из строки str.
 
-// var str = "    Max is a good      boy     ";
-
-// function strip(str) {
-//     ...ваш код...
+// let str = '    Max is a good      boy     ';
+// let strip = (str) => {
+//   return str.trim().replace(/\s+/g, ' ');
 // };
+
+// console.log(strip(str));  //  Max is a good boy
 
 ////////////////////////////////////////////////////////////
 // 22. Удалить лишние слова из строки
 // Напишите функцию cutString(str, n), которая удаляет лишние слова из строки str, оставив в ней n слов.
 
-// var str = "Сила тяжести приложена к центру масс тела";
-
-// function cutString(str, n) {
-//     ...ваш код...
+// let str = 'Сила тяжести приложена к центру масс тела';
+// let cutString = (str, n) => {
+//   return str.split(' ').splice(0, n).join(' ');
 // };
+
+// console.log(cutString(str, 2));  //  Сила тяжести
 
 ////////////////////////////////////////////////////////////
 // 23. Найти слово в строке
 // Напишите функцию findWord(word, str), которая проверяет, существует ли в строке str слова word.
 
-// var str = 'abc def ghi jkl mno pqr stu';
-
-// function findWord(word, str) {
-//     ...ваш код...
+// let str = 'abc def ghi jkl mno pqr stu';
+// let findWord = (word, str) => {
+//   return str.includes(word);
 // };
+
+// console.log(findWord('stu', str)); //  true
+// console.log(findWord('sta', str)); //  false
